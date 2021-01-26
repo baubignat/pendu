4d46bc5b14e4f5b2560a05cc8d52d29e3b6f8f49
Benjamin Aubignat <73789133+baubignat@users.noreply.github.com>
2021-01-26 19:02:46 +0100

    Delete pendu.h

--- pendu.h
+++
@@ -1,8 +0,0 @@
-#define DICO
-#ifdef DICO_ANIMALS
-dictionnaire = fopen ( "dico_animals.txt" , r ) ;
-#ifdef DICO_COUNTRIES
-dictionnaire = fopen ( "dico_countries.txt" , r ) ;
-#ifdef DICO_MOVIES
-dictionnaire = fopen ( "dico_movies.txt" , r ) ;
-#endif
